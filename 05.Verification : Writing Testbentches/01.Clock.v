bdd7688884355aa7d1e59ac84b12e64a99b801e4
MAHENDIRAN S <mahemaahi07@gmail.com>
2026-03-11 20:06:28 +0530

    Create 01.Clock.v

---
+++ 05.Verification : Writing Testbentches/01.Clock.v
@@ -0,0 +1,12 @@
+module top_module;
+    reg clk;
+    dut uut (
+        .clk(clk)
+    );
+    initial begin
+        clk = 0;
+    end
+    always begin
+        #5 clk = ~clk;
+    end
+endmodule
